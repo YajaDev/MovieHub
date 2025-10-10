60c6e19b4a8fbe8076e80a029f315bf0306dd9f4
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-10-10 15:31:04 +0800

    Create Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar"
 
 function App() {
-return (
-  <>
-  </>
-)
+  return (
+    <div className="min-h-screen px-[5%]">
+      <header className="flex justify-between items-center py-3 md:py-5">
+        <Navbar />
+      </header>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Menu, Search } from "lucide-react";
+
+const Navbar = () => {
+  
+  return (
+    <>
+      <div className="text-2xl font-bold">
+        Movie<span className="text-primary">Hub</span>
+      </div>
+
+      {/* Navigation Bar */}
+      <nav className="hidden md:flex gap-5">
+        <a href="#home">Home</a>
+        <a href="#tranding">Trending</a>
+        <a href="#popular">Popular</a>
+        <a href="#topRated">Top Rated</a>
+      </nav>
+
+      {/* Search Bar */}
+      <div className="hidden md:block relative text-foreground">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="bg-foreground/15 px-4 py-2 text-sm rounded-full w-40 focus:w-50 lg:w-50 lg:focus:w-65 transition-all"
+        />
+        <Search className="absolute top-2.5 right-3 h-4 w-4" />
+      </div>
+
+      <Menu className="md:hidden m-3 text-primary" size={17} strokeWidth={4}/>
+    </>
+  );
+};
+
+export default Navbar;
