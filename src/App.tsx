5f13265a0d3d827a3786b2f10e3901e8698844ad
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-11-05 23:58:34 +0800

    fixed placing of moviemodal provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/header/Header";
 import MainLayout from "./components/layout/MainLayout";
 import { MovieProvider } from "./context/MovieContext";
+import { MovieModalProvider } from "./context/MovieModalContext";
 
 function App() {
 
   return (
     <MovieProvider>
-      <Header />
+      <MovieModalProvider>
+        <Header />
       <MainLayout />
-      <Footer />
+        <Footer />
+      </MovieModalProvider>
     </MovieProvider>
   );
 }
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,4 @@
 import { useMovieContext } from "../../context/MovieContext";
-import { MovieModalProvider } from "../../context/MovieModalContext";
 import GenreSection from "../sections/GenreSection";
 import HeroSection from "../sections/HeroSection";
 import MovieSlider from "../sections/MovieSlider";
@@ -9,36 +8,34 @@ const MainLayout = () => {
   const { trending, popular, top_rated } = useMovieContext();
 
   return (
-    <MovieModalProvider>
-      <main id="home">
-        <HeroSection />
-        <div className="px-[6%] md:px-[8%] space-y-5">
-          <MovieSlider
-            id="trending"
-            title="Trending This Week"
-            subTitle="Stay updated with everyone's watching"
-            moviesData={trending}
-          />
-          <MovieSlider
-            id="popular"
-            title="Popular Movies"
-            subTitle="Most watched movies right now"
-            moviesData={popular}
-          />
+    <main id="home">
+      <HeroSection />
+      <div className="px-[6%] md:px-[8%] space-y-5">
+        <MovieSlider
+          id="trending"
+          title="Trending This Week"
+          subTitle="Stay updated with everyone's watching"
+          moviesData={trending}
+        />
+        <MovieSlider
+          id="popular"
+          title="Popular Movies"
+          subTitle="Most watched movies right now"
+          moviesData={popular}
+        />
 
-          <GenreSection />
+        <GenreSection />
 
-          <MovieSlider
-            id="topRated"
-            title="Top Rated Movies"
-            subTitle="Highest rated movies of all time"
-            moviesData={top_rated}
-          />
+        <MovieSlider
+          id="topRated"
+          title="Top Rated Movies"
+          subTitle="Highest rated movies of all time"
+          moviesData={top_rated}
+        />
 
-          <MovieDetails />
-        </div>
-      </main>
-    </MovieModalProvider>
+        <MovieDetails />
+      </div>
+    </main>
   );
 };
 
